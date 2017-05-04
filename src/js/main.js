924efbce2efb8fa67bb642e376a839ac916324d0
John Robert Pett <john@containerstud.io>
2017-05-04 09:21:35 +0200

    Intro to jQuery.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 console.log('Hello world!');
+
+$(document).ready(function() {
+	console.log('ready');
+});
+
+$(window).resize(function() {
+	console.log('resize');
+});
+
+$(window).scroll(function() {
+	console.log('scroll');
+});
